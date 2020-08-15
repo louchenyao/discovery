bce349c4e92ee83e4efd72257d60fbb445ef83e9
louchenyao <louchenyao@gmail.com>
2020-08-15 17:31:06 +0800

    it get stucked when typing a lot in debug mode.

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -2,16 +2,77 @@
 #![no_main]
 #![no_std]
 
+use core::fmt::{self, Write};
+use heapless::{consts, Vec, String};
+
 #[allow(unused_imports)]
-use aux11::{entry, iprint, iprintln};
+use aux11::{entry, iprint, iprintln, usart1};
+
+macro_rules! uprint {
+    ($serial:expr, $($arg:tt)*) => {
+        $serial.write_fmt(format_args!($($arg)*)).ok()
+    };
+}
+
+macro_rules! uprintln {
+    ($serial:expr, $fmt:expr) => {
+        uprint!($serial, concat!($fmt, "\n"))
+    };
+    ($serial:expr, $fmt:expr, $($arg:tt)*) => {
+        uprint!($serial, concat!($fmt, "\n"), $($arg)*)
+    };
+}
+
+struct SerialPort {
+    usart1: &'static mut usart1::RegisterBlock,
+}
+
+impl fmt::Write for SerialPort {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for c in s.bytes() {
+            while self.usart1.isr.read().txe().bit_is_clear() {}
+            self.usart1.tdr.write(|w| w.tdr().bits(u16::from(c)));
+        }
+        Ok(())
+    }
+}
+
+impl SerialPort {
+    fn recv_byte(&mut self) -> u8 {
+        while self.usart1.isr.read().rxne().bit_is_clear() {}
+        self.usart1.rdr.read().rdr().bits() as u8
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    let (usart1, /*mono_timer,*/ mut itm) = aux11::init();
-    iprintln!(&mut itm.stim[0], "init success");
+    let (usart1, /* mono_timer, */ mut itm) = aux11::init();
+    let mut serial = SerialPort { usart1 };
+    let mut buffer: Vec<u8, consts::U32> = Vec::new();
 
-    // Send a single character
-    usart1.tdr.write(|w| w.tdr().bits(u16::from(b'X')));
+    loop {
+        buffer.clear();
+        loop {
+            let b = serial.recv_byte() as char;
+            iprintln!(&mut itm.stim[0], "received {}", b);
+            match b {
+                '\n' => {
+                    let s = String::from_utf8(buffer).unwrap();
+                    uprintln!(serial, "{}", s.as_str());
+                    buffer = s.into_bytes();
+                    break;
+                }
+                b => {
+                    match buffer.push(b as u8) {
+                        Ok(_) => (),
+                        Err(e) => {
+                            uprintln!(serial, "cannot push {}", e as char);
+                            break;
+                        }
+                    }
+                }
+            }
 
-    loop {}
+        }
+    }
 }
